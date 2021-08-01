33b0a1677b68a31374761fe44698d1b74278cfa0
gabe <ggetzie@gmail.com>
2021-08-01 09:42:41 +0800

    mapholder, photos json

---
+++ lib/types.ts
@@ -0,0 +1,15 @@
+export interface Photo {
+    id: number,
+    title: string,
+    credit: string,
+    country: string,
+    state: string,
+    city: string,
+    latitude: number,
+    longitude: number,
+    min_year: number,
+    max_year: number
+}
+
+
+
