f270134d366d38ad0cea457e748abc4957ad6a63
Gabe Getzie <ggetzie@gmail.com>
2021-08-02 22:05:10 +0800

    markers

--- components/Map.tsx
+++ components/Map.tsx
@@ -1,3 +1,4 @@
+import { FC } from 'react';
 import { MapContainer, Marker, Popup, TileLayer } from 'react-leaflet'
 import 'leaflet/dist/leaflet.css'
 import 'leaflet-defaulticon-compatibility/dist/leaflet-defaulticon-compatibility.webpack.css'; // Re-uses images from ~leaflet package
@@ -5,9 +6,17 @@ import * as L from 'leaflet';
 import 'leaflet-defaulticon-compatibility';
 
 import { LatLngTuple } from 'leaflet';
+import  { Photo } from '../lib/types';
 
-function Map() {
+interface MapProps {
+    photos: Photo[]
+}
+
+const Map: FC<MapProps> = ({ photos  }) => {
     const startPosition : LatLngTuple = [37.09024, -95.712891];
+    const markers = photos.map((p: Photo, i: number) => (<Marker key={i} position={[p.latitude, p.longitude]}></Marker>
+    ))
+    console.log(photos);
 
     return (
         <MapContainer center={startPosition} zoom={4} scrollWheelZoom={true} 
@@ -16,11 +25,8 @@ function Map() {
                 attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
                 url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
             />
-            <Marker position={startPosition}>
-                <Popup>
-                A pretty CSS3 popup. <br /> Easily customizable.
-                </Popup>
-            </Marker>
+            {/* <Marker position={startPosition}></Marker> */}
+            {markers}
         </MapContainer>
     )
 }
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,11 +1,24 @@
 /* eslint-disable react/display-name */
+import { useState } from 'react';
 import Head from 'next/head';
 import Image from 'next/image';
 import styles from '../styles/Home.module.css';
 import dynamic from 'next/dynamic';
+import { Photo } from '../lib/types';
 
 const STATIC_URL = 'https://d2sgv5kjr4yd0f.cloudfront.net';
-export default function Home() {
+export default function Home(
+  { locatedPhotos,  minYear,  maxYear}: {
+    locatedPhotos: Photo[],
+    minYear: number,
+    maxYear: number
+  }
+  ) {
+
+  const [allPhotos, setAllPhotos] = useState(locatedPhotos);
+  const [selectedPhotos, setSelectedPhotos] = useState(locatedPhotos)
+  const [fromYear, setFromYear] = useState(minYear);
+  const [toYear, setToYear] = useState(maxYear);
 
   const Map = dynamic(
     () => import('../components/Map'), 
@@ -24,7 +37,7 @@ export default function Home() {
       </Head>
 
       <main className={styles.main}>
-        <Map />
+        <Map photos={selectedPhotos} />
       </main>
 
       <footer className={styles.footer}>
@@ -32,4 +45,18 @@ export default function Home() {
       </footer>
     </div>
   )
+}
+
+export async function getStaticProps() {
+  const photos: Photo[] = require('../data/photos.json');
+  const locatedPhotos: Photo[] =  photos.filter((p: Photo) => p.latitude !== null && p.longitude !== null);
+  const minYear: number = Math.min(...locatedPhotos.map((p: Photo) => p.min_year));
+  const maxYear: number = Math.max(...locatedPhotos.map((p: Photo) => p.max_year));
+  return {
+    props: {
+      locatedPhotos,
+      minYear,
+      maxYear,
+    }
+  }
 }
